d694ce1e3de9195b043fd50db4747b014474914c
Alejandro Sánchez <alex.sanzb@gmail.com>
2021-10-22 12:41:57 +0200

    Addedsticky checkout box

--- components/ProductView.tsx
+++ components/ProductView.tsx
@@ -42,8 +42,7 @@ const ProductView = ({ product }: GetProductQuery) => {
   useScrollPosition(
     ({ currPos }) => {
       console.log(currPos.y);
-      setIsSticky(currPos.y >= 230);
-      setIsShadowed(currPos.y >= 250);
+      setIsShadowed(currPos.y >= 288);
     },
     [isSticky, isShadowed],
     undefined,
@@ -52,171 +51,170 @@ const ProductView = ({ product }: GetProductQuery) => {
   );
   return (
     <>
-      <aside className="container flex items-start justify-between w-full mx-auto">
-        <div className="flex justify-center px-4">
-          <div className="relative flex flex-col items-start justify-start mr-4">
-            {product.imageGallery.images.map((image: any, idx: number) => (
-              <div
-                key={image.icon.substr(image.icon.length - 16)}
-                className={clsx('w-18 h-18 mb-4', idx !== 0 && 'opacity-50')}
-              >
-                <Image
-                  src={image.icon}
-                  alt={image.icon || 'Product Image'}
-                  width={100}
-                  height={100}
-                  layout="fixed"
-                  objectFit="contain"
-                  blurDataURL={image.icon}
+      <div className="container flex-col items-start py-8 mx-auto">
+        <div className="justify-items-end inline-grid w-full grid-cols-2">
+          <div className="flex justify-center px-4">
+            <div className="relative flex flex-col items-start justify-start mr-4">
+              {product.imageGallery.images.map((image: any, idx: number) => (
+                <div
+                  key={image.icon.substr(image.icon.length - 16)}
+                  className={clsx('w-18 h-18 mb-4', idx !== 0 && 'opacity-50')}
+                >
+                  <Image
+                    src={image.icon}
+                    alt={image.icon || 'Product Image'}
+                    width={100}
+                    height={100}
+                    layout="fixed"
+                    objectFit="contain"
+                    blurDataURL={image.icon}
+                  />
+                </div>
+              ))}
+            </div>
+            <Image
+              src={product.imageGallery.images[0].image}
+              alt={product.title || 'Product Image'}
+              width={600}
+              height={600}
+              layout="fixed"
+              objectFit="contain"
+              blurDataURL={product.imageGallery.images[0].image}
+            />
+          </div>
+          <aside className="">
+            <div className="w-96 flex flex-col px-6">
+              <p className="py-2 text-xs text-gray-600">
+                {product.categories[0].parent && product.categories[0].parent.parent
+                  ? `${product.categories[0].parent.parent.title} / `
+                  : null}
+                {product.categories[0].parent ? `${product.categories[0].parent.title} / ` : null}
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: product.categories[0].title,
+                  }}
+                ></span>
+              </p>
+              <div className="py-2 border-b border-gray-200">
+                <h2
+                  className="text-2xl font-bold"
+                  dangerouslySetInnerHTML={{
+                    __html: product.title,
+                  }}
+                />
+                <p
+                  className="py-1 text-sm font-light text-gray-500"
+                  dangerouslySetInnerHTML={{
+                    __html: product.shortDescription ?? '',
+                  }}
                 />
               </div>
-            ))}
-          </div>
-          <Image
-            src={product.imageGallery.images[0].image}
-            alt={product.title || 'Product Image'}
-            width={600}
-            height={600}
-            layout="fixed"
-            objectFit="contain"
-            blurDataURL={product.imageGallery.images[0].image}
-          />
-        </div>
-        <div className="relative flex flex-col">
-          <div className="w-96 flex flex-col px-6">
-            <p className="py-2 text-xs text-gray-600">
-              {product.categories[0].parent && product.categories[0].parent.parent
-                ? `${product.categories[0].parent.parent.title} / `
-                : null}
-              {product.categories[0].parent ? `${product.categories[0].parent.title} / ` : null}
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: product.categories[0].title,
-                }}
-              ></span>
-            </p>
-            <div className="py-2 border-b border-gray-200">
-              <h2
-                className="text-2xl font-bold"
-                dangerouslySetInnerHTML={{
-                  __html: product.title,
-                }}
-              />
-              <p
-                className="py-1 text-sm font-light text-gray-500"
-                dangerouslySetInnerHTML={{
-                  __html: product.shortDescription ?? '',
-                }}
-              />
             </div>
-          </div>
-          <div
-            className={clsx(
-              'flex flex-col w-96 p-6 sticky -top-96',
-              isSticky && '',
-              isShadowed && 'rounded-sm shadow-2xl'
-            )}
-          >
-            <div className="flex items-start py-1 space-x-1">
-              <div className="flex items-start justify-start text-sm">
-                <span className="text-2xl font-semibold">{Math.trunc(product.price.price)}</span>
-                <span className="text-gray-800">
-                  {`.${(product.price.price.toFixed(2) + '').split('.')[1]}`}
-                </span>
+            <div
+              className={clsx(
+                'flex flex-col w-96 p-6 sticky mt-6 top-36',
+                isShadowed && 'rounded-sm shadow-xl'
+              )}
+            >
+              <div className="flex items-start py-1 space-x-1">
+                <div className="flex items-start justify-start text-sm">
+                  <span className="text-2xl font-semibold">{Math.trunc(product.price.price)}</span>
+                  <span className="text-gray-800">
+                    {`.${(product.price.price.toFixed(2) + '').split('.')[1]}`}
+                  </span>
+                </div>
+                <span className="text-sm text-gray-600">{product.price.currency.sign}</span>
               </div>
-              <span className="text-sm text-gray-600">{product.price.currency.sign}</span>
-            </div>
 
-            {product.variants && product.variantLabels
-              ? product.variantLabels.map((label: string, idx: any) => (
-                  <div key={label}>
-                    <div className="flex items-baseline justify-between">
-                      <div>
-                        <span className="text-sm font-light capitalize">{`${label}: `}</span>
-                        <span className="text-sm font-medium">
-                          {label === 'Color'
-                            ? state.color
-                            : label === 'EU-Size'
-                            ? state.size
-                            : null}
-                        </span>
-                      </div>
-                      {label === 'EU-Size' && (
-                        <button className="float-right text-xs font-light underline">
-                          Size Guide
-                        </button>
-                      )}
-                    </div>
-                    <div
-                      className={clsx(
-                        'flex items-center py-1 space-x-2',
-                        label === 'EU-Size' && 'justify-between'
-                      )}
-                    >
-                      {getFilterVariants(idx).map((variant: any) => (
-                        <div
-                          key={variant}
-                          className={clsx(
-                            'px-6 py-2 border border-gray-400 rounded-lg',
-                            state.color === variant && 'border-black',
-                            state.size === variant && 'border-black'
-                          )}
-                        >
-                          {label.toLowerCase() === 'color' ? (
-                            <div className="relative w-16 h-16">
-                              <Image
-                                src={getFilterVariantsImages(variant)[0]}
-                                alt={getFilterVariantsImages(variant)[0] || 'Product Image'}
-                                width={100}
-                                height={100}
-                                layout="fixed"
-                                objectFit="contain"
-                                blurDataURL={getFilterVariantsImages(variant)[0]}
-                              />
-
-                              {state.color === variant ? (
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  className="-right-3 -bottom-1 absolute w-6 h-6"
-                                  viewBox="0 0 20 20"
-                                  fill="currentColor"
-                                >
-                                  <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                    clipRule="evenodd"
-                                  />
-                                </svg>
-                              ) : null}
-                            </div>
-                          ) : (
-                            <p className="text-sm">{variant}</p>
-                          )}
+              {product.variants && product.variantLabels
+                ? product.variantLabels.map((label: string, idx: any) => (
+                    <div key={label}>
+                      <div className="flex items-baseline justify-between">
+                        <div>
+                          <span className="text-sm font-light capitalize">{`${label}: `}</span>
+                          <span className="text-sm font-medium">
+                            {label === 'Color'
+                              ? state.color
+                              : label === 'EU-Size'
+                              ? state.size
+                              : null}
+                          </span>
                         </div>
-                      ))}
-                    </div>
-                  </div>
-                ))
-              : null}
-            <div className="flex w-full pt-6 space-x-2">
-              <div className="rounded-3 flex justify-around w-20 py-4 text-sm border border-gray-300">
-                <div className="text-gray-300" role="button" tabIndex={0}>
-                  -
-                </div>
-                1
-                <div className="text-black" role="button" tabIndex={0}>
-                  +
-                </div>
-              </div>
-              <button className="rounded-3 flex-1 py-4 text-sm font-light text-white bg-black">
-                Add to cart
-              </button>
-            </div>
-          </div>
-        </div>
-      </aside>
+                        {label === 'EU-Size' && (
+                          <button className="float-right text-xs font-light underline">
+                            Size Guide
+                          </button>
+                        )}
+                      </div>
+                      <div
+                        className={clsx(
+                          'flex items-center py-1 space-x-2',
+                          label === 'EU-Size' && 'justify-between'
+                        )}
+                      >
+                        {getFilterVariants(idx).map((variant: any) => (
+                          <div
+                            key={variant}
+                            className={clsx(
+                              'px-6 py-2 border border-gray-400 rounded-lg',
+                              state.color === variant && 'border-black',
+                              state.size === variant && 'border-black'
+                            )}
+                          >
+                            {label.toLowerCase() === 'color' ? (
+                              <div className="relative w-16 h-16">
+                                <Image
+                                  src={getFilterVariantsImages(variant)[0]}
+                                  alt={getFilterVariantsImages(variant)[0] || 'Product Image'}
+                                  width={100}
+                                  height={100}
+                                  layout="fixed"
+                                  objectFit="contain"
+                                  blurDataURL={getFilterVariantsImages(variant)[0]}
+                                />
 
-      <div className="container flex flex-col items-center justify-between mx-auto">
+                                {state.color === variant ? (
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="-right-3 -bottom-1 absolute w-6 h-6"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                  >
+                                    <path
+                                      fillRule="evenodd"
+                                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                      clipRule="evenodd"
+                                    />
+                                  </svg>
+                                ) : null}
+                              </div>
+                            ) : (
+                              <p className="text-sm">{variant}</p>
+                            )}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  ))
+                : null}
+              <div className="flex w-full pt-6 space-x-2">
+                <div className="rounded-3 flex justify-around w-20 py-4 text-sm border border-gray-300">
+                  <div className="text-gray-300" role="button" tabIndex={0}>
+                    -
+                  </div>
+                  1
+                  <div className="text-black" role="button" tabIndex={0}>
+                    +
+                  </div>
+                </div>
+                <button className="rounded-3 flex-1 py-4 text-sm font-light text-white bg-black">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          </aside>
+        </div>
+
         <div className="w-full py-8">
           <h2 className="text-xl font-semibold text-gray-700 capitalize">Related Products</h2>
           <GridView items={product.crossSelling} />
