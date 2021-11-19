6f0688f2065e8a19d2bd00f9c40a2c03623d44ca
Alejandro Sánchez <alex.sanzb@gmail.com>
2021-11-19 10:14:57 +0100

    fix: range map return type

--- lib/range-map.ts
+++ lib/range-map.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-const rangeMap = (n: number, fn: (i: number) => any) => {
+const rangeMap = (n: number, fn: (i: number) => any): any[] => {
   const arr = [];
   while (n > arr.length) {
     arr.push(fn(arr.length));
