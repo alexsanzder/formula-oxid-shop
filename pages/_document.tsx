8b18c30049e9e4f84414c2e93a24fe036c93e53c
Alejandro Sánchez <alex.sanzb@gmail.com>
2021-11-21 00:39:59 +0100

    fix: change next document import name

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-interface Props {}
-
-class MyDocument extends Document<Props> {
+class MyDocument extends Document {
   render() {
     return (
       <Html>
