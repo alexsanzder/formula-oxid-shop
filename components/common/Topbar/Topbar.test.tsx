6c90baafa1135d0376a5ffd4e276ff49d69713fa
Alejandro Sánchez <alex.sanzb@gmail.com>
2021-11-21 01:23:23 +0100

    test: update topbar test add user-event

--- components/common/Topbar/Topbar.test.tsx
+++ components/common/Topbar/Topbar.test.tsx
@@ -1,12 +1,12 @@
 import Topbar from '@components/common/Topbar';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 
 describe('Topbar', () => {
   it('should render topbar and dismiss it', () => {
     render(<Topbar />);
     expect(screen.getByRole('alert')).toBeInTheDocument();
-
-    fireEvent.click(screen.getByRole('button', { name: /Dismiss topbar/i }));
+    user.click(screen.getByRole('button', { name: /Dismiss topbar/i }));
     expect(screen.queryByRole('alert')).not.toBeInTheDocument();
   });
 });
