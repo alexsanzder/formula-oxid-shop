ef1536bc8c9a2fe5ddb94060d5e1ef93053a112d
Alejandro Sánchez <alex.sanzb@gmail.com>
2021-12-15 14:25:14 +0100

    fix: update graphql endpoint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['formula.localhost', 'oc-core-dev.oxid.dev'],
+    domains: ['formula.localhost', 'oc-core-dev.oxid.dev', 'oc-core-dev-integration.oxid.dev'],
     pageExtensions: ['page.tsx'],
   },
 };
